3fc21d6013dcfff32dcea532314d4fdedd1f2577
mpicadorph <mpicado@educa.jcyl.es>
2024-11-11 21:44:20 +0100

    contrasedfna

---
+++ contrasena/script.js
@@ -0,0 +1,9 @@
+let contraseña="hola";
+let palabra=" ";
+palabra=prompt("Introduce la clave de acceso al contenido:");
+if (palabra ===contraseña){
+  alert("Contraseña correcta");
+}
+  else{
+  alert("Introduce de nuevo la contraseña");
+}
