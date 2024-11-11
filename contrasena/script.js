96160dbb72160eb906a42dd2aa4188d7efe3d493
mpicadorph <mpicado@educa.jcyl.es>
2024-11-11 22:22:12 +0100

    FVDVDF

--- contrasena/script.js
+++
@@ -1,9 +0,0 @@
-let contraseña = "hola";
-let palabra = " ";
-palabra = prompt("Introduce la clave de acceso al contenido:");
-if (palabra === contraseña) {
-    alert("Contraseña correcta");
-}
-else {
-    alert("Introduce de nuevo la contraseña");
-}
