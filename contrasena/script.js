ca029f9bfe3620a45c1d4d4a0e47683291090a68
mpicadorph <mpicado@educa.jcyl.es>
2024-11-11 21:49:05 +0100

    df

--- contrasena/script.js
+++ contrasena/script.js
@@ -1,9 +1,9 @@
-let contraseña="hola";
-let palabra=" ";
-palabra=prompt("Introduce la clave de acceso al contenido:");
-if (palabra ===contraseña){
-  alert("Contraseña correcta");
+let contraseña = "hola";
+let palabra = " ";
+palabra = prompt("Introduce la clave de acceso al contenido:");
+if (palabra === contraseña) {
+    alert("Contraseña correcta");
 }
-  else{
-  alert("Introduce de nuevo la contraseña");
+else {
+    alert("Introduce de nuevo la contraseña");
 }
